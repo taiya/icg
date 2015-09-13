a70009d39ff9b6b8d7e8badf09a65b99847fede9
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2015-09-13 16:41:24 -0700

    Intro to C++ (code of monday's lecture)

---
+++ introcpp/Shape.cpp
@@ -0,0 +1,35 @@
+#include "Shape.h"
+
+#include <iostream>
+using namespace std;
+
+Shape::Shape(int r, int g, int b){
+    cout << "Shape()" << endl;
+    _color = new int[3]; ///< RGB          
+    _color[0]=r; _color[1]=g; _color[2]=b;
+}
+
+Shape::~Shape(){ 
+    cout << "~Shape()" << endl;
+    delete[] _color; 
+}
+
+Shape::Shape(const Shape &shape){
+    cout << "Shape(Shape& )" << endl;
+    this->_size = shape._size;
+    _color = new int[3]; ///< CAREFUL!
+    for (int i = 0; i < 3; ++i) 
+        this->_color[i] = shape._color[i];
+}
+
+Shape &Shape::operator=(const Shape &shape){
+    cout << "Shape::operator=" << endl;
+    this->_size = shape._size;
+    for (int i = 0; i < 3; ++i) 
+        this->_color[i] = shape._color[i];
+}
+
+void Shape::operator*(const float scale){
+    cout << "Shape::operator*" << endl;
+    this->_size *= scale;
+}
